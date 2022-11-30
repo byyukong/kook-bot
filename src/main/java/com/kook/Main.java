dfefa39cfc724d95190b26497dc2357dd41dbc88
gaojj <woxihuandazhuza1>
2022-11-30 09:51:04 +0800

    a

--- src/main/java/com/kook/Main.java
+++ src/main/java/com/kook/Main.java
@@ -22,7 +22,7 @@ import java.util.Map;
 public class Main extends BasePlugin {
     @Override
     public void onLoad() {
-        // 重写 onLoad 并不是必须的，但是您可以在此阶段做一些初始化工作，比如解压默认的配置文件 (saveDefaultConfig)
+        // 重写 onLoad 并不是必须的，但是您可以在此阶段做一些初始化工作，比如解压默认的配置文件 (saveDefaultConfig)1
         // 三个方法内的日志记录也不是必须的，但是善用日志记录有助于描述 Bot 的运行状态！
         getLogger().info("PingBot 加载完成！"); // getLogger() 返回的是一个 org.slf4j.Logger 的实例对象，为日志记录器
     }
