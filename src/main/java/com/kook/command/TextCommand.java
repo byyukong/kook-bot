b40e0b55681192e69d6f66f5331f5857a8df1e2f
ast <3314068135@qq.com>
2022-12-02 17:38:49 +0800

    修改命令:今天吃什么

--- src/main/java/com/kook/command/TextCommand.java
+++ src/main/java/com/kook/command/TextCommand.java
@@ -77,6 +77,7 @@ public class TextCommand extends BasePlugin {
         assert jsonObject != null;
         if (!"200".equals(jsonObject.get("code"))){
             getLogger().error("报错:"+"200".equals(jsonObject.get("code")));
+            getLogger().error("报错类型:"+jsonObject.get("code").getClass());
             return "异常了"+jsonObject.get("code");
         }
         return jsonObject.getJSONArray("data").get(0).toString();
