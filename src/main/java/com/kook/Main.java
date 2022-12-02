27215ba09d53d8511c284a60180f880de165da01
acc1o <1636579550@qq.com>
2022-12-02 16:19:13 +0800

    摸鱼图片Api

--- src/main/java/com/kook/Main.java
+++ src/main/java/com/kook/Main.java
@@ -6,8 +6,11 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.kook.pojo.weather.ResultsVo;
 import com.kook.util.OkHttpClientUtil;
+import com.kook.util.PictureUtils;
+import snw.jkook.JKook;
 import snw.jkook.command.JKookCommand;
 import snw.jkook.entity.User;
+import snw.jkook.entity.abilities.Accessory;
 import snw.jkook.message.Message;
 import snw.jkook.message.TextChannelMessage;
 import snw.jkook.message.component.BaseComponent;
@@ -16,11 +19,15 @@ import snw.jkook.message.component.card.CardBuilder;
 import snw.jkook.message.component.card.MultipleCardComponent;
 import snw.jkook.message.component.card.Size;
 import snw.jkook.message.component.card.Theme;
+import snw.jkook.message.component.card.element.ImageElement;
+import snw.jkook.message.component.card.element.MarkdownElement;
 import snw.jkook.message.component.card.element.PlainTextElement;
 import snw.jkook.message.component.card.module.ContextModule;
 import snw.jkook.message.component.card.module.HeaderModule;
+import snw.jkook.message.component.card.module.SectionModule;
 import snw.jkook.plugin.BasePlugin;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -237,6 +244,39 @@ public class Main extends BasePlugin {
                         }
                 )
                 .register();
+
+        new JKookCommand("摸鱼")
+                .executesUser(
+                        (sender, args, message) -> {
+                            if (sender instanceof User) {
+                                Map<String,Object> res = OkHttpClientUtil.get("https://api.vvhan.com/api/moyu?type=json");
+                                try {
+                                    File file = PictureUtils.UrlToFile(res.get("url").toString());
+                                    MultipleCardComponent card = new CardBuilder()
+                                            .setTheme(Theme.NONE)
+                                            .setSize(Size.LG)
+                                            .addModule(new HeaderModule(new PlainTextElement("!!!", false)))
+                                            .addModule(new SectionModule(new MarkdownElement("摸鱼图片:")
+                                                    ,file != null ? new ImageElement(
+                                                            JKook.getHttpAPI().uploadFile(file),
+                                                    null,
+                                                    Size.SM,
+                                                    false
+                                            ):null,
+                                                    file !=null ? Accessory.Mode.RIGHT : null))
+                                            .build();
+                                    reply(sender, message, card);
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                            } else {
+                                getLogger().info("This command is not available for console.");
+                            }
+                        }
+                )
+                .register();
+        
+        
 
 
         getLogger().info("PingBot 启动成功！");
---
+++ src/main/java/com/kook/util/PictureUtils.java
@@ -0,0 +1,23 @@
+package com.kook.util;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * @author Suww
+ * @createTime 2022-12-02
+ */
+public class PictureUtils {
+    public static File UrlToFile(String url) throws Exception {
+            HttpURLConnection httpUrl = (HttpURLConnection) new URL(url).openConnection();
+            httpUrl.connect();
+            InputStream ins=httpUrl.getInputStream();
+            File file = new File(System.getProperty("java.io.tmpdir") + File.separator + "xia");
+            if (file.exists()) {
+                file.delete();//如果缓存中存在该文件就删除
+            }
+            OutputStream os = new FileOutputStream(file);
+            return file;
+    }
+}
