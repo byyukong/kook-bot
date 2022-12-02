d38bf50be56cfb6d99a95563c121968d2b74df48
acc1o <1636579550@qq.com>
2022-12-02 16:26:16 +0800

    删除无用代码

--- src/main/java/com/kook/util/PictureUtils.java
+++ src/main/java/com/kook/util/PictureUtils.java
@@ -12,7 +12,6 @@ public class PictureUtils {
     public static File UrlToFile(String url) throws Exception {
             HttpURLConnection httpUrl = (HttpURLConnection) new URL(url).openConnection();
             httpUrl.connect();
-            InputStream ins=httpUrl.getInputStream();
             File file = new File(System.getProperty("java.io.tmpdir") + File.separator + "xia");
             if (file.exists()) {
                 file.delete();//如果缓存中存在该文件就删除
